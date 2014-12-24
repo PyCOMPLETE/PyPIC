71bc2bd46097cbb908cae7eee2edbc5fe2d4cea8
giadarol <giovanni.iadarola@cern.ch>
2014-12-24 17:30:19 +0100

    it workks :-) but still in script form

--- 002_test_rect_chamber.py
+++ 002_test_rect_chamber.py
@@ -8,10 +8,10 @@ import numpy as np
 from module_poisson_from_exmple import fft_poisson
 
 x_aper = .5e-1
-y_aper = .25e-1
+y_aper = 2e-1
 R_charge = 4e-2
 N_part_gen = 100000
-Dh = 2e-3
+Dh = 1e-3
 
 from scipy.constants import e, epsilon_0
 
@@ -79,7 +79,7 @@ j_max = np.max(np.where(yg<y_aper)[0])+1
 
 
 phi = 0*picFDSW.rho
-phi[i_min:i_max,j_min:j_max] = fft_poisson(-picFDSW.rho[i_min:i_max,j_min:j_max]/eps0*np.pi**2, Dh)
+phi[i_min:i_max,j_min:j_max] = fft_poisson(-picFDSW.rho[i_min:i_max,j_min:j_max]/eps0, Dh)
 
 pl.figure(100)
 pl.pcolor(picFDSW.phi.T)
