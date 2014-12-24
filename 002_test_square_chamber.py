3cf201a9396de4491fba0e894c037284474e1fee
giadarol <giovanni.iadarola@cern.ch>
2014-12-24 11:53:26 +0100

    spotted another factor pi**2
    the remainig factor seems to be dependent on the size of the cavity

--- 002_test_square_chamber.py
+++ 002_test_square_chamber.py
@@ -1,5 +1,4 @@
 import FiniteDifferences_ShortleyWeller_SquareGrid as PIC_FDSW
-import FiniteDifferences_Staircase_SquareGrid as PIC_FD
 import FFT_OpenBoundary_SquareGrid as PIC_FFT
 import geom_impact_ellip as ell
 import geom_impact_poly as poly
@@ -8,10 +7,10 @@ import numpy as np
 
 from module_poisson_from_exmple import fft_poisson
 
-R_cham = 1e-1
+R_cham = .5e-1
 R_charge = 4e-2
 N_part_gen = 100000
-Dh = 2e-3
+Dh = .5e-3
 
 from scipy.constants import e, epsilon_0
 
@@ -26,7 +25,6 @@ chamber = poly.polyg_cham_geom_object({'Vx':na([R_cham, -R_cham, -R_cham, R_cham
 									   'x_sem_ellip_insc':0.99*R_cham,
 									   'y_sem_ellip_insc':0.99*R_cham})
 
-picFD = PIC_FD.FiniteDifferences_Staircase_SquareGrid(chamb = chamber, Dh = Dh)
 picFDSW = PIC_FDSW.FiniteDifferences_ShortleyWeller_SquareGrid(chamb = chamber, Dh = Dh)
 picFFT = PIC_FFT.FFT_OpenBoundary_SquareGrid(x_aper = chamber.x_aper, y_aper = chamber.y_aper, Dh = Dh)
 
@@ -41,12 +39,10 @@ y_part = y_part[mask_keep]
 nel_part = 0*x_part+1.
 
 #pic scatter
-picFD.scatter(x_part, y_part, nel_part)
 picFDSW.scatter(x_part, y_part, nel_part)
 picFFT.scatter(x_part, y_part, nel_part)
 
 #pic scatter
-picFD.solve()
 picFDSW.solve()
 picFFT.solve()
 
@@ -54,7 +50,6 @@ x_probes = np.linspace(0,R_cham,1000)
 y_probes = 0.*x_probes
 
 #pic gather
-Ex_FD, Ey_FD = picFD.gather(x_probes, y_probes)
 Ex_FDSW, Ey_FDSW = picFDSW.gather(x_probes, y_probes)
 Ex_FFT, Ey_FFT = picFFT.gather(x_probes, y_probes)
 
@@ -63,7 +58,6 @@ E_r_th = map(lambda x: -np.sum(x_part**2+y_part**2<x**2)*qe/eps0/(2*np.pi*x), x_
 
 import pylab as pl
 pl.close('all')
-pl.plot(x_probes, Ex_FD, label = 'FD')
 pl.plot(x_probes, Ex_FDSW, label = 'FD ShorleyWeller')
 pl.plot(x_probes, Ex_FFT, label = 'FFT open')
 pl.plot(x_probes, E_r_th, label = 'Analytic')
@@ -73,8 +67,8 @@ pl.ylabel('Ex on the x axis [V/m]')
 pl.xlabel('x [m]')
 
 # I try to use with the dst solver with the correct boundary
-xg = picFD.xg
-yg = picFD.yg
+xg = picFDSW.xg
+yg = picFDSW.yg
 x_aper = chamber.x_aper
 y_aper = chamber.y_aper
 
@@ -84,15 +78,15 @@ j_min = np.min(np.where(yg>-y_aper)[0])
 j_max = np.max(np.where(yg<y_aper)[0])+1
 
 
-phi = 0*picFD.rho
-phi[i_min:i_max,j_min:j_max] = fft_poisson(-picFD.rho[i_min:i_max,j_min:j_max]/eps0*Dh, Dh)
+phi = 0*picFDSW.rho
+phi[i_min:i_max,j_min:j_max] = fft_poisson(-picFDSW.rho[i_min:i_max,j_min:j_max]/eps0*Dh*np.pi**2, Dh)
 
 pl.figure(100)
-pl.pcolor(picFD.phi)
+pl.pcolor(picFDSW.phi)
 
 pl.figure(101)
 pl.pcolor(phi)
-pl.suptitle('%f'%(np.sum(phi)/np.sum(picFD.phi)))
+pl.suptitle('%f'%(np.sum(picFDSW.phi)/np.sum(phi)))
 
 
 pl.show()
