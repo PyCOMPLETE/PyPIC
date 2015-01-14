ae220921c8ccbfac1d694f4fc9c258bec3e35469
giadarol <giovanni.iadarola@cern.ch>
2015-01-14 13:49:40 +0100

    modification from tune spread exercise for USPAS

--- PyPIC_Scatter_Gather.py
+++ PyPIC_Scatter_Gather.py
@@ -64,21 +64,33 @@ class PyPIC_Scatter_Gather(object):
 	
 	__metadata__ = ABCMeta
 	
-	def __init__(self, x_aper, y_aper, Dh, *args, **kwargs):
+	def __init__(self, x_aper=None, y_aper=None, Dh=None, xg=None, yg=None, *args, **kwargs):
 		print 'Call Generic Constructor'
-		xg=np.arange(0, x_aper+5.*Dh,Dh,float)  
-		xgr=xg[1:]
-		xgr=xgr[::-1]#reverse array
-		xg=np.concatenate((-xgr,xg),0)
-		Nxg=len(xg);
-		bias_x=min(xg);
+		
+		if xg!=None and xg!=None:
+			assert(x_aper==None and y_aper==None and Dh==None)
+			Nxg=len(xg);
+			bias_x=min(xg);
 
-		yg=np.arange(0,y_aper+4.*Dh,Dh,float)  
-		ygr=yg[1:]
-		ygr=ygr[::-1]#reverse array
-		yg=np.concatenate((-ygr,yg),0)
-		Nyg=len(yg);
-		bias_y=min(yg);
+			Nyg=len(yg);
+			bias_y=min(yg);
+			
+			Dh = xg[1]-xg[0]
+		else:
+			assert(xg==None and xg==None)
+			xg=np.arange(0, x_aper+5.*Dh,Dh,float)  
+			xgr=xg[1:]
+			xgr=xgr[::-1]#reverse array
+			xg=np.concatenate((-xgr,xg),0)
+			Nxg=len(xg);
+			bias_x=min(xg);
+
+			yg=np.arange(0,y_aper+4.*Dh,Dh,float)  
+			ygr=yg[1:]
+			ygr=ygr[::-1]#reverse array
+			yg=np.concatenate((-ygr,yg),0)
+			Nyg=len(yg);
+			bias_y=min(yg);
 
 		self.Dh=Dh
 		self.xg = xg
@@ -90,7 +102,7 @@ class PyPIC_Scatter_Gather(object):
 
                         
 	#@profile
-	def scatter(self, x_mp, y_mp, nel_mp):
+	def scatter(self, x_mp, y_mp, nel_mp, charge = -qe):
 		
 		if not (len(x_mp)==len(y_mp)==len(nel_mp)):
 			raise ValueError('x_mp, y_mp, nel_mp should have the same length!!!')
@@ -99,7 +111,9 @@ class PyPIC_Scatter_Gather(object):
 			rho=rhocom.compute_sc_rho(x_mp,y_mp,nel_mp,
 									  self.bias_x,self.bias_y,self.Dh, self.Nxg, self.Nyg)
 
-			self.rho=-qe*rho/(self.Dh*self.Dh);
+			self.rho=charge*rho/(self.Dh*self.Dh);
+		else:
+			self.rho=self.rho*0.
 
          
 	def gather(self, x_mp, y_mp):
@@ -125,6 +139,6 @@ class PyPIC_Scatter_Gather(object):
 		self.efx, self.efy.'''
 		pass
 		
-	def scatter_and_solve(self, x_mp, y_mp, nel_mp):
-		self.scatter(x_mp, y_mp, nel_mp)
+	def scatter_and_solve(self, x_mp, y_mp, nel_mp, charge = -qe):
+		self.scatter(x_mp, y_mp, nel_mp, charge)
 		self.solve()
