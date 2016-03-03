a160d29a2a679eaaf9748e631eaa8dbfec2a29da
like2000 <kevin.li.cern@gmail.com>
2016-03-03 11:39:27 +0100

    Get PyPIC to compile also on systems that are not exclusively Ubuntu - such as e.g. OpenSUSE...

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from distutils.core import setup, Extension
+import numpy as np
+
+from setuptools import setup, Extension
+
+from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(Extension(
-           "CyFPPS",                                # the extesion name
-           sources=['FPPS/CyFPPS.pyx', 'FPPS/FPPSWrapper.cc',
-           'FPPS/ChangeCoord.cc', 'FPPS/ElectricFieldSolver.cc', 'FPPS/Mesh.cc',
-		   'FPPS/ChangeCoord_Frac.cc', 'FPPS/FastPolarPoissonSolver.cc',  'FPPS/NonLinearMesh.cc',
-		   'FPPS/ChangeCoord_Tanh.cc', 'FPPS/PolarBeamRepresentation.cc',
-		   'FPPS/ChargeDistribution.cc', 'FPPS/FunctionsFPPS.cc'], 				  # the Cython source and additional C++ source files generate and compile C++ code
-           language="c++",  libraries=[	'fftw3']                      
-      )))
+
+cy_ext_options = {"compiler_directives": {"profile": True}, "annotate": True}
+cy_ext = [
+    Extension(
+        "CyFPPS",
+        sources=[ # the Cython source and additional C++ source files generate and compile C++ code
+            'FPPS/CyFPPS.pyx', 'FPPS/FPPSWrapper.cc',
+            'FPPS/ChangeCoord.cc', 'FPPS/ElectricFieldSolver.cc', 'FPPS/Mesh.cc',
+	    'FPPS/ChangeCoord_Frac.cc', 'FPPS/FastPolarPoissonSolver.cc',  'FPPS/NonLinearMesh.cc',
+	    'FPPS/ChangeCoord_Tanh.cc', 'FPPS/PolarBeamRepresentation.cc',
+	    'FPPS/ChargeDistribution.cc', 'FPPS/FunctionsFPPS.cc'],
+        language="c++", include_dirs=[np.get_include()], libraries=['fftw3', 'm'])
+]
+
+
+setup(
+    name='PyPIC',
+    description='Collection of Python Particle-In-Cell solvers.',
+    url='http://github.com/PyCOMPLETE/PyPIC',
+    packages=['PyPIC'],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize(cy_ext, **cy_ext_options),
+    install_requires=[
+        'numpy',
+        'cython'
+    ]
+)
