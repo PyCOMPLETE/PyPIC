68d084218f4530adba29a60c364462e6c293be64
elebelli <eleonora.belli@cern.ch>
2016-06-24 11:17:32 +0200

    Solve states for FDSC

--- FiniteDifferences_Staircase_SquareGrid.py
+++ FiniteDifferences_Staircase_SquareGrid.py
@@ -64,9 +64,9 @@ qe = e
 eps0 = epsilon_0
 
 class FiniteDifferences_Staircase_SquareGrid(PyPIC_Scatter_Gather):
-    #@profile
-    def __init__(self, chamb, Dh, sparse_solver = 'scipy_slu', remove_external_nodes_from_mat=True):
-        
+	#@profile
+	def __init__(self, chamb, Dh, sparse_solver = 'scipy_slu', remove_external_nodes_from_mat=True, include_solver = True):
+		
 		print 'Start PIC init.:'
 		print 'Finite Differences, Square Grid'
 
@@ -100,93 +100,96 @@ class FiniteDifferences_Staircase_SquareGrid(PyPIC_Scatter_Gather):
 		gradmod=abs(gx)+abs(gy);
 		flag_border_mat=np.logical_and((gradmod>0), flag_outside_n_mat);
 		flag_border_n = flag_border_mat.flatten()
-
-		A=scsp.lil_matrix((Nxg*Nyg,Nxg*Nyg)); #allocate a sparse matrix
-
-		list_internal_force_zero = []
-
-		# Build A matrix
-		for u in range(0,Nxg*Nyg):
-			if np.mod(u, Nxg*Nyg/20)==0:
-				print ('Mat. assembly %.0f'%(float(u)/ float(Nxg*Nyg)*100)+"""%""")
-			if flag_inside_n[u]:
-				A[u,u] = -(4./(Dh*Dh))
-				A[u,u-1]=1./(Dh*Dh);     #phi(i-1,j)nx
-				A[u,u+1]=1./(Dh*Dh);     #phi(i+1,j)
-				A[u,u-Nyg]=1./(Dh*Dh);    #phi(i,j-1)
-				A[u,u+Nyg]=1./(Dh*Dh);    #phi(i,j+1)
-			else:
-				# external nodes
-				A[u,u]=1.
-				
-		A=A.tocsr() #convert to csr format
 		
-		#Remove trivial equtions 
-		if remove_external_nodes_from_mat:
-			diagonal = A.diagonal()
-			N_full = len(diagonal)
-			indices_non_id = np.where(diagonal!=1.)[0]
-			N_sel = len(indices_non_id)
-			Msel = scsp.lil_matrix((N_full, N_sel))
-			for ii, ind in enumerate(indices_non_id):
-				Msel[ind, ii] =1.
-		else:
-			diagonal = A.diagonal()
-			N_full = len(diagonal)
-			Msel = scsp.lil_matrix((N_full, N_full))
-			for ii in xrange(N_full):
-				Msel[ii, ii] =1.
-		Msel = Msel.tocsc()
-		Asel = Msel.T*A*Msel
-		Asel=Asel.tocsc() 
 		
+		if include_solver:
+			A=scsp.lil_matrix((Nxg*Nyg,Nxg*Nyg)); #allocate a sparse matrix
 
-		if sparse_solver == 'scipy_slu':
-			print "Using scipy superlu solver..."
-			luobj = ssl.splu(Asel.tocsc())
-		elif sparse_solver == 'PyKLU':
-			print "Using klu solver..."
-			try:
-				import PyKLU.klu as klu
-				luobj = klu.Klu(Asel.tocsc())
-			except StandardError, e: 
-				print "Got exception: ", e
-				print "Falling back on scipy superlu solver:"
-				luobj = ssl.splu(Asel.tocsc())
-		else:
-			raise ValueError('Solver not recognized!!!!\nsparse_solver must be "scipy_slu" or "PyKLU"\n')
+			list_internal_force_zero = []
+
+			# Build A matrix
+			for u in range(0,Nxg*Nyg):
+				if np.mod(u, Nxg*Nyg/20)==0:
+					print ('Mat. assembly %.0f'%(float(u)/ float(Nxg*Nyg)*100)+"""%""")
+				if flag_inside_n[u]:
+					A[u,u] = -(4./(Dh*Dh))
+					A[u,u-1]=1./(Dh*Dh);     #phi(i-1,j)nx
+					A[u,u+1]=1./(Dh*Dh);     #phi(i+1,j)
+					A[u,u-Nyg]=1./(Dh*Dh);    #phi(i,j-1)
+					A[u,u+Nyg]=1./(Dh*Dh);    #phi(i,j+1)
+				else:
+					# external nodes
+					A[u,u]=1.
+					
+			A=A.tocsr() #convert to csr format
 			
-		self.xn = xn
-		self.yn = yn
-		
-		self.flag_inside_n = flag_inside_n
-		self.flag_outside_n = flag_outside_n
-		self.flag_outside_n_mat = flag_outside_n_mat
-		self.flag_inside_n_mat = np.logical_not(flag_outside_n_mat)
-		self.flag_border_mat = flag_border_mat
-		self.Asel = Asel
-		self.luobj = luobj
+			#Remove trivial equtions 
+			if remove_external_nodes_from_mat:
+				diagonal = A.diagonal()
+				N_full = len(diagonal)
+				indices_non_id = np.where(diagonal!=1.)[0]
+				N_sel = len(indices_non_id)
+				Msel = scsp.lil_matrix((N_full, N_sel))
+				for ii, ind in enumerate(indices_non_id):
+					Msel[ind, ii] =1.
+			else:
+				diagonal = A.diagonal()
+				N_full = len(diagonal)
+				Msel = scsp.lil_matrix((N_full, N_full))
+				for ii in xrange(N_full):
+					Msel[ii, ii] =1.
+			Msel = Msel.tocsc()
+			Asel = Msel.T*A*Msel
+			Asel=Asel.tocsc() 
+			
+
+			if sparse_solver == 'scipy_slu':
+				print "Using scipy superlu solver..."
+				luobj = ssl.splu(Asel.tocsc())
+			elif sparse_solver == 'PyKLU':
+				print "Using klu solver..."
+				try:
+					import PyKLU.klu as klu
+					luobj = klu.Klu(Asel.tocsc())
+				except StandardError, e: 
+					print "Got exception: ", e
+					print "Falling back on scipy superlu solver:"
+					luobj = ssl.splu(Asel.tocsc())
+			else:
+				raise ValueError('Solver not recognized!!!!\nsparse_solver must be "scipy_slu" or "PyKLU"\n')
+				
+			self.xn = xn
+			self.yn = yn
+			
+			self.flag_inside_n = flag_inside_n
+			self.flag_outside_n = flag_outside_n
+			self.flag_outside_n_mat = flag_outside_n_mat
+			self.flag_inside_n_mat = np.logical_not(flag_outside_n_mat)
+			self.flag_border_mat = flag_border_mat
+			self.Asel = Asel
+			self.luobj = luobj
+			self.U_sc_eV_stp=0.;
+			self.sparse_solver = sparse_solver
+			self.Msel = Msel.tocsc()
+			self.Msel_T = (Msel.T).tocsc()
+			self.flag_border_n = flag_border_n
+			
+			print 'Done PIC init.'
+			
+		else:
+			self.solve = self._solve_for_states          
 
 
 		self.rho = np.zeros((self.Nxg,self.Nyg));
 		self.phi = np.zeros((self.Nxg,self.Nyg));
 		self.efx = np.zeros((self.Nxg,self.Nyg));
 		self.efy = np.zeros((self.Nxg,self.Nyg));
-
-		
-		self.U_sc_eV_stp=0.;
-
-		
-		self.Msel = Msel.tocsc()
-		self.Msel_T = (Msel.T).tocsc()
-		
-		self.flag_border_n = flag_border_n
 		self.chamb = chamb
-		print 'Done PIC init.'
-                        
-
-    #@profile    
-    def solve(self, rho = None, flag_verbose = False, pic_external = None):
+		
+		
+		
+	#@profile    
+	def solve(self, rho = None, flag_verbose = False, pic_external = None):
 
 		if rho == None:
 			rho = self.rho
@@ -232,6 +235,69 @@ class FiniteDifferences_Staircase_SquareGrid(PyPIC_Scatter_Gather):
 		self.b = b
 		self.phi = phi
 		self.U_sc_eV_stp = U_sc_eV_stp
-        
+		
+	def get_state_object(self):
+		state = FiniteDifferences_Staircase_SquareGrid(chamb=self.chamb, Dh=self.Dh, include_solver=False)
+		
+		state.rho = self.rho.copy()
+		state.phi = self.phi.copy()
+		state.efx = self.efx.copy()
+		state.efy = self.efy.copy()
+		
+		return state		
+		
+	def solve_states(self, states, pic_external = None):
+		
+		states = np.atleast_1d(states)
+		
+		if len(states) == 1:
+			state = states[0]
+			self._solve_core(state, state.rho, pic_external)		
+			
+		else:
+			for ii in xrange(len(states)):
+				state = states[ii]
+				self._solve_core(state, state.rho, pic_external)
+				
+				
+	def _solve_core(self, state, rho, pic_external):
+
+		b=-rho.flatten()/eps0;
+		b[~(self.flag_inside_n)]=0.; #boundary condition
+
+		if pic_external is not None:
+			x_border = self.xn[self.flag_border_n]
+			y_border = self.yn[self.flag_border_n]
+			phi_border = pic_external.gather_phi(x_border, y_border)
+			b[self.flag_border_n] = phi_border
+
+		b_sel = self.Msel_T*b
+		phi_sel = self.luobj.solve(b_sel)
+		phi = self.Msel*phi_sel
+		phi=np.reshape(phi,(self.Nxg,self.Nyg))
+
+		efx = state.efx
+		efy = state.efy
+
+		efx[1:self.Nxg-1,:] = phi[0:self.Nxg-2,:] - phi[2:self.Nxg,:];  #central difference on internal nodes
+		efy[:,1:self.Nyg-1] = phi[:,0:self.Nyg-2] - phi[:,2:self.Nyg];  #central difference on internal nodes
+
+		efx[self.flag_border_mat]=efx[self.flag_border_mat]*2;
+		efy[self.flag_border_mat]=efy[self.flag_border_mat]*2;
+		
+		state.efx = efx / (2*self.Dh);    #divide grid size
+		state.efy = efy / (2*self.Dh); 
+		state.rho = rho
+		state.phi = phi
+		state.b = b
+		
+
+		
+		
+
+
+
+
+
 
 
