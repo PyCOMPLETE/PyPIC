edd8839775eb5d368c52cac72557fa2498a0a0db
giadarol <giovanni.iadarola@cern.ch>
2016-07-04 19:38:07 +0200

    Finalized example

--- 012_test_states.py
+++ 012_test_states.py
@@ -11,7 +11,7 @@ chamber = ell.ellip_cham_geom_object(x_aper = x_aper, y_aper = y_aper)
 
 #build particle distribution
 from scipy import randn
-N_part = 10000; sigmax=1e-3; sigmay=2e-3
+N_part = 10000; sigmax=.5e-3; sigmay=1e-3
 x_mp = sigmax*randn(N_part);
 y_mp = sigmay*randn(N_part);
 nel_mp = x_mp*0.+1.
@@ -23,62 +23,88 @@ theta_probes = np.linspace(0, 2*np.pi, N_probes)
 x_probes = n_sigma_probes*sigmax*np.cos(theta_probes)
 y_probes = n_sigma_probes*sigmay*np.sin(theta_probes)
 
-# build pic
+# build pics
+
+pic_list = []
+
+# Finite Difference Shortley-Weller
 Dh=1e-3
 import FiniteDifferences_ShortleyWeller_SquareGrid as PIC_FDSW
-pic = PIC_FDSW.FiniteDifferences_ShortleyWeller_SquareGrid(chamb = chamber, Dh = Dh)
+pic_list.append(PIC_FDSW.FiniteDifferences_ShortleyWeller_SquareGrid(chamb = chamber, Dh = Dh, sparse_solver = 'PyKLU'))
 
-# standard solve and gather
-pic.scatter(x_mp, y_mp, nel_mp, charge = qe)
-pic.solve()
-Ex_probes, Ey_probes = pic.gather(x_probes, y_probes)
+# Finite Difference Staircase
+Dh=1e-3
+import FiniteDifferences_Staircase_SquareGrid as PIC_FDSC
+pic_list.append(PIC_FDSC.FiniteDifferences_Staircase_SquareGrid(chamb = chamber, Dh = Dh, sparse_solver = 'PyKLU'))
 
+#  Multi grid 
+Sx_target = 5*sigmax
+Sy_target = 5*sigmay
+Dh_target = 0.1*min([sigmax, sigmay])
+Dh_single = .5e-3
+pic_singlegrid = PIC_FDSW.FiniteDifferences_ShortleyWeller_SquareGrid(chamb = chamber, Dh = Dh_single, sparse_solver = 'PyKLU')
+from MultiGrid import AddTelescopicGrids
+pic_list.append(AddTelescopicGrids(pic_main = pic_singlegrid, f_telescope = 0.3, 
+    target_grid = {'x_min_target':-Sx_target/2., 'x_max_target':Sx_target/2.,'y_min_target':-Sy_target/2.,'y_max_target':Sy_target/2.,'Dh_target':Dh_target}, 
+    N_nodes_discard = 3., N_min_Dh_main = 10, sparse_solver='PyKLU'))
 
+# test:
 
-# build state list
-fact_states = np.linspace(1.5, 2., 4)
-N_states = len(fact_states)
-
-list_states = []
-for _ in fact_states:
-    list_states.append(pic.get_state_object())
-    
-for i_state, state in enumerate(list_states):
-    state.scatter(x_mp, y_mp, nel_mp*fact_states[i_state], charge = qe)
- 
-# solve states
-pic.solve_states(list_states)
-
-# gather and plot
 pl.close('all')
-pl.figure(1)
-pl.subplot(2,1,1)
-pl.plot(theta_probes, Ex_probes)
-pl.subplot(2,1,2)
-pl.plot(theta_probes, Ey_probes)
+ms.mystyle_arial(fontsz = 14)
 
-for i_state, state in enumerate(list_states):
-    colorcurr = ms.colorprog(i_state, N_states)
-    Ex_prb_state, Ey_prb_state = state.gather(x_probes, y_probes)
+for i_pic, pic in enumerate(pic_list):
+    
+    # standard solve and gather
+    pic.scatter(x_mp, y_mp, nel_mp, charge = qe)
+    pic.solve()
+    Ex_probes, Ey_probes = pic.gather(x_probes, y_probes)
+
+    # build state list
+    fact_states = np.linspace(1.5, 2., 4)
+    N_states = len(fact_states)
+
+    list_states = []
+    for _ in fact_states:
+        list_states.append(pic.get_state_object())
+        
+    for i_state, state in enumerate(list_states):
+        state.scatter(x_mp, y_mp, nel_mp*fact_states[i_state], charge = qe)
+     
+    # solve states
+    pic.solve_states(list_states)
+
+    # gather and plot
+    pl.figure(1+i_pic)
     pl.subplot(2,1,1)
-    pl.plot(theta_probes, Ex_prb_state, '.', color=colorcurr)
-    pl.plot(theta_probes, Ex_probes*fact_states[i_state], '-', color=colorcurr)
+    pl.plot(theta_probes, Ex_probes)
     pl.subplot(2,1,2)
-    pl.plot(theta_probes, Ey_prb_state, '.', color=colorcurr)
-    pl.plot(theta_probes, Ey_probes*fact_states[i_state], '-', color=colorcurr)
+    pl.plot(theta_probes, Ey_probes)
 
-#check single state case
-fact_single_state = 2.5
-single_state = pic.get_state_object()
-single_state.scatter(x_mp, y_mp, nel_mp*fact_single_state, charge = qe)
-pic.solve_states(single_state)
-Ex_prb_single_state, Ey_prb_single_state = single_state.gather(x_probes, y_probes)
-colorcurr = 'black'
-pl.subplot(2,1,1)
-pl.plot(theta_probes, Ex_prb_single_state, '.', color=colorcurr)
-pl.plot(theta_probes, Ex_probes*fact_single_state, '-', color=colorcurr)
-pl.subplot(2,1,2)
-pl.plot(theta_probes, Ey_prb_single_state, '.', color=colorcurr)
-pl.plot(theta_probes, Ey_probes*fact_single_state, '-', color=colorcurr)
+    for i_state, state in enumerate(list_states):
+        colorcurr = ms.colorprog(i_state, N_states)
+        Ex_prb_state, Ey_prb_state = state.gather(x_probes, y_probes)
+        pl.subplot(2,1,1)
+        pl.plot(theta_probes, Ex_prb_state, '.', color=colorcurr)
+        pl.plot(theta_probes, Ex_probes*fact_states[i_state], '-', color=colorcurr)
+        pl.subplot(2,1,2)
+        pl.plot(theta_probes, Ey_prb_state, '.', color=colorcurr)
+        pl.plot(theta_probes, Ey_probes*fact_states[i_state], '-', color=colorcurr)
+
+    #check single state case
+    fact_single_state = 2.5
+    single_state = pic.get_state_object()
+    single_state.scatter(x_mp, y_mp, nel_mp*fact_single_state, charge = qe)
+    pic.solve_states(single_state)
+    Ex_prb_single_state, Ey_prb_single_state = single_state.gather(x_probes, y_probes)
+    colorcurr = 'black'
+    pl.subplot(2,1,1)
+    pl.plot(theta_probes, Ex_prb_single_state, '.', color=colorcurr)
+    pl.plot(theta_probes, Ex_probes*fact_single_state, '-', color=colorcurr)
+    pl.subplot(2,1,2)
+    pl.plot(theta_probes, Ey_prb_single_state, '.', color=colorcurr)
+    pl.plot(theta_probes, Ey_probes*fact_single_state, '-', color=colorcurr)
+    
+    pl.suptitle(str(pic.__class__).replace('<','').replace('>','').split('.')[-1].replace("'", ''))
 
 pl.show()
