a4019920a5be14944dd3bb34e88ece1af4c88063
Adrian Oeftiger <oeftiger@cern.ch>
2016-12-01 16:32:18 +0100

    added verbose flags to avoid cluttering the console.

--- Bassetti_Erskine.py
+++ Bassetti_Erskine.py
@@ -1,53 +1,53 @@
 #----------------------------------------------------------------------
-#                                                                      
-#                           CERN                                       
-#                                                                      
-#     European Organization for Nuclear Research                       
-#                                                                      
-#     
-#     This file is part of the code:
-#                                                                      		    
-# 
-#		           PyPIC Version 2.0.0                     
-#                  
-#                                                                       
-#     Author and contact:   Giovanni IADAROLA 
-#                           BE-ABP Group                               
-#                           CERN                                       
-#                           CH-1211 GENEVA 23                          
-#                           SWITZERLAND  
-#                           giovanni.iadarola@cern.ch                  
-#                                                                      
-#                contact:   Giovanni RUMOLO                            
-#                           BE-ABP Group                               
-#                           CERN                                      
-#                           CH-1211 GENEVA 23                          
-#                           SWITZERLAND  
-#                           giovanni.rumolo@cern.ch                    
-#                                                                      
 #
-#                                                                      
-#     Copyright  CERN,  Geneva  2011  -  Copyright  and  any   other   
-#     appropriate  legal  protection  of  this  computer program and   
-#     associated documentation reserved  in  all  countries  of  the   
-#     world.                                                           
-#                                                                      
-#     Organizations collaborating with CERN may receive this program   
-#     and documentation freely and without charge.                     
-#                                                                      
-#     CERN undertakes no obligation  for  the  maintenance  of  this   
-#     program,  nor responsibility for its correctness,  and accepts   
-#     no liability whatsoever resulting from its use.                  
-#                                                                      
-#     Program  and documentation are provided solely for the use  of   
-#     the organization to which they are distributed.                  
-#                                                                      
-#     This program  may  not  be  copied  or  otherwise  distributed   
-#     without  permission. This message must be retained on this and   
-#     any other authorized copies.                                     
-#                                                                      
-#     The material cannot be sold. CERN should be  given  credit  in   
-#     all references.                                                  
+#                           CERN
+#
+#     European Organization for Nuclear Research
+#
+#
+#     This file is part of the code:
+#
+#
+#		           PyPIC Version 2.0.0
+#
+#
+#     Author and contact:   Giovanni IADAROLA
+#                           BE-ABP Group
+#                           CERN
+#                           CH-1211 GENEVA 23
+#                           SWITZERLAND
+#                           giovanni.iadarola@cern.ch
+#
+#                contact:   Giovanni RUMOLO
+#                           BE-ABP Group
+#                           CERN
+#                           CH-1211 GENEVA 23
+#                           SWITZERLAND
+#                           giovanni.rumolo@cern.ch
+#
+#
+#
+#     Copyright  CERN,  Geneva  2011  -  Copyright  and  any   other
+#     appropriate  legal  protection  of  this  computer program and
+#     associated documentation reserved  in  all  countries  of  the
+#     world.
+#
+#     Organizations collaborating with CERN may receive this program
+#     and documentation freely and without charge.
+#
+#     CERN undertakes no obligation  for  the  maintenance  of  this
+#     program,  nor responsibility for its correctness,  and accepts
+#     no liability whatsoever resulting from its use.
+#
+#     Program  and documentation are provided solely for the use  of
+#     the organization to which they are distributed.
+#
+#     This program  may  not  be  copied  or  otherwise  distributed
+#     without  permission. This message must be retained on this and
+#     any other authorized copies.
+#
+#     The material cannot be sold. CERN should be  given  credit  in
+#     all references.
 #----------------------------------------------------------------------
 
 import numpy as np
@@ -61,25 +61,38 @@ eps0 = epsilon_0
 
 class Interpolated_Bassetti_Erskine(PyPIC_Scatter_Gather):
     #@profile
-    def __init__(self, x_aper, y_aper, Dh, sigmax, sigmay, 
-        n_imag_ellip=0, tot_charge=1., verbose=True, allow_scatter_and_solve=False):
-        
+    def __init__(self, x_aper, y_aper, Dh=None, sigmax=None, sigmay=None,
+            n_imag_ellip=0, tot_charge=1., verbose=True,
+            allow_scatter_and_solve=False, dx=None, dy=None):
+        # dx and dy added ad-hoc in the spirit of FFT_OpenBoundary
+
         self.verbose = verbose
         self.allow_scatter_and_solve = allow_scatter_and_solve
-        
+
         if self.verbose:
             print 'Start PIC init.:'
-            print 'Bassetti-Erskine, Square Grid'
 
-        self.Dh = Dh
-        super(Interpolated_Bassetti_Erskine, self).__init__(x_aper, y_aper, self.Dh, self.Dh, verbose=self.verbose)
-        
+        if dx!=None and dy!=None:
+            assert(Dh==None)
+            if self.verbose:
+                print 'Bassetti-Erskine, Rectangular Grid'
+
+        elif Dh!=None:
+            assert(dx==None and dy==None)
+            dx = Dh
+            dy = Dh
+            if self.verbose:
+                print 'Bassetti-Erskine, Square Grid'
+
+        super(Interpolated_Bassetti_Erskine, self).__init__(
+            x_aper, y_aper, dx, dy, verbose=self.verbose)
+
         xx = self.xg
         yy = self.yg
-        
+
         Ex=np.zeros((len(xx),len(yy)),dtype=complex);
         Ey=np.zeros((len(xx),len(yy)),dtype=complex);
-        
+
         for ii in range(len(xx)):
 
             if np.mod(ii, len(xx)/20)==0 and self.verbose:
@@ -92,24 +105,24 @@ class Interpolated_Bassetti_Erskine(PyPIC_Scatter_Gather):
                 Ex_BE,Ey_BE      = BassErsk(x,y,sigmax,sigmay)
                 Ex[ii,jj] = Ex_BE + Ex_imag
                 Ey[ii,jj] = Ey_BE + Ey_imag
-                
-        YY,XX = np.meshgrid(self.yg, self.xg)		
+
+        YY,XX = np.meshgrid(self.yg, self.xg)
         self.rho = tot_charge/(2.*np.pi*sigmax*sigmay)*np.exp(-(XX)**2/(2.*sigmax**2)-(YY)**2/(2.*sigmay**2))
         self.phi = np.zeros((self.Nxg,self.Nyg))
         self.efx = tot_charge * Ex.real
         self.efy = tot_charge * Ey.real
-                        
 
-    #@profile    
+
+    #@profile
     def solve(self, rho = None, flag_verbose = False):
         if not self.allow_scatter_and_solve:
             raise ValueError('Bassetti_Erskine: nothing to solve!!!!')
-        
+
     def scatter(self,  x_mp, y_mp, nel_mp, charge = -qe):
         if not self.allow_scatter_and_solve:
             raise ValueError('Bassetti_Erskine: what do you want to scatter???!!!!')
-        
-        
+
+
 
 
 def wfun(z):
@@ -119,65 +132,65 @@ def wfun(z):
     return wx+1j*wy
 
 def BassErsk(xin,yin,sigmax,sigmay):
-        
+
     x=abs(xin);
     y=abs(yin);
-    
-    
-    
+
+
+
     if sigmax>sigmay:
-    
+
         S=np.sqrt(2*(sigmax*sigmax-sigmay*sigmay));
         factBE=1/(2*eps0*np.sqrt(np.pi)*S);
         etaBE=sigmay/sigmax*x+1j*sigmax/sigmay*y;
         zetaBE=x+1j*y;
-        
+
         val=factBE*(wfun(zetaBE/S)-np.exp( -x*x/(2*sigmax*sigmax)-y*y/(2*sigmay*sigmay))*wfun(etaBE/S) );
-           
+
         Ex=abs(val.imag)*np.sign(xin);
         Ey=abs(val.real)*np.sign(yin);
-    
+
     else:
-    
+
         S=np.sqrt(2*(sigmay*sigmay-sigmax*sigmax));
         factBE=1/(2*eps0*np.sqrt(np.pi)*S);
         etaBE=sigmax/sigmay*y+1j*sigmay/sigmax*x;
         yetaBE=y+1j*x;
-        
+
         val=factBE*(wfun(yetaBE/S)-np.exp( -y*y/(2*sigmay*sigmay)-x*x/(2*sigmax*sigmax))*wfun(etaBE/S) );
-           
+
         Ey=abs(val.imag)*np.sign(yin);
         Ex=abs(val.real)*np.sign(xin);
-         
+
     return Ex, Ey
 
 def ImageTerms(x,y,a,b,x0,y0, nimag):
-    
-        
-    eps0=epsilon_0;    
-    
-    if nimag>0 and abs((a-b)/a)>1e-3:    
+
+
+    eps0=epsilon_0;
+
+    if nimag>0 and abs((a-b)/a)>1e-3:
         g=np.sqrt(a*a-b*b)
         z=x+1j*y
         q=np.arccosh(z/g)
         mu=q.real
         phi=q.imag
-        
+
         z0=x0+1j*y0
         q0=np.arccosh(z0/g)
         mu0=q0.real
         phi0=q0.imag
-        
+
         mu1=0.5*np.log((a+b)/(a-b))
-        
+
         Ecpx=0+0j
-        
-        
+
+
         q=np.conj(q)
         for nn in range(1,nimag+1):
             Ecpx=Ecpx+np.exp(-nn*mu1) * ( (np.cosh(nn*mu0)*np.cos(nn*phi0)) / (np.cosh(nn*mu1)) + 1j * (np.sinh(nn*mu0)*np.sin(nn*phi0)) / (np.sinh(nn*mu1))   )* (np.sinh(nn*q))/(np.sinh(q))
-            
-        
+
+
         Ecpx=Ecpx/(4*np.pi*eps0)*4/g
         Ex=Ecpx.real
         Ey=Ecpx.imag
@@ -187,7 +200,7 @@ def ImageTerms(x,y,a,b,x0,y0, nimag):
             Ey=0.
         else:
             print('This case has not been implemented yet')
-    
+
     return Ex, Ey
 
 
--- FFT_OpenBoundary.py
+++ FFT_OpenBoundary.py
@@ -1,53 +1,53 @@
 #----------------------------------------------------------------------
-#                                                                      
-#                           CERN                                       
-#                                                                      
-#     European Organization for Nuclear Research                       
-#                                                                      
-#     
-#     This file is part of the code:
-#                                                                      		    
-# 
-#		           PyPIC Version 2.0.0                     
-#                  
-#                                                                       
-#     Author and contact:   Giovanni IADAROLA 
-#                           BE-ABP Group                               
-#                           CERN                                       
-#                           CH-1211 GENEVA 23                          
-#                           SWITZERLAND  
-#                           giovanni.iadarola@cern.ch                  
-#                                                                      
-#                contact:   Giovanni RUMOLO                            
-#                           BE-ABP Group                               
-#                           CERN                                      
-#                           CH-1211 GENEVA 23                          
-#                           SWITZERLAND  
-#                           giovanni.rumolo@cern.ch                    
-#                                                                      
 #
-#                                                                      
-#     Copyright  CERN,  Geneva  2011  -  Copyright  and  any   other   
-#     appropriate  legal  protection  of  this  computer program and   
-#     associated documentation reserved  in  all  countries  of  the   
-#     world.                                                           
-#                                                                      
-#     Organizations collaborating with CERN may receive this program   
-#     and documentation freely and without charge.                     
-#                                                                      
-#     CERN undertakes no obligation  for  the  maintenance  of  this   
-#     program,  nor responsibility for its correctness,  and accepts   
-#     no liability whatsoever resulting from its use.                  
-#                                                                      
-#     Program  and documentation are provided solely for the use  of   
-#     the organization to which they are distributed.                  
-#                                                                      
-#     This program  may  not  be  copied  or  otherwise  distributed   
-#     without  permission. This message must be retained on this and   
-#     any other authorized copies.                                     
-#                                                                      
-#     The material cannot be sold. CERN should be  given  credit  in   
-#     all references.                                                  
+#                           CERN
+#
+#     European Organization for Nuclear Research
+#
+#
+#     This file is part of the code:
+#
+#
+#                   PyPIC Version 2.0.0
+#
+#
+#     Author and contact:   Giovanni IADAROLA
+#                           BE-ABP Group
+#                           CERN
+#                           CH-1211 GENEVA 23
+#                           SWITZERLAND
+#                           giovanni.iadarola@cern.ch
+#
+#                contact:   Giovanni RUMOLO
+#                           BE-ABP Group
+#                           CERN
+#                           CH-1211 GENEVA 23
+#                           SWITZERLAND
+#                           giovanni.rumolo@cern.ch
+#
+#
+#
+#     Copyright  CERN,  Geneva  2011  -  Copyright  and  any   other
+#     appropriate  legal  protection  of  this  computer program and
+#     associated documentation reserved  in  all  countries  of  the
+#     world.
+#
+#     Organizations collaborating with CERN may receive this program
+#     and documentation freely and without charge.
+#
+#     CERN undertakes no obligation  for  the  maintenance  of  this
+#     program,  nor responsibility for its correctness,  and accepts
+#     no liability whatsoever resulting from its use.
+#
+#     Program  and documentation are provided solely for the use  of
+#     the organization to which they are distributed.
+#
+#     This program  may  not  be  copied  or  otherwise  distributed
+#     without  permission. This message must be retained on this and
+#     any other authorized copies.
+#
+#     The material cannot be sold. CERN should be  given  credit  in
+#     all references.
 #----------------------------------------------------------------------
 
 import numpy as np
@@ -65,119 +65,126 @@ eps0=epsilon_0
 
 class FFT_OpenBoundary(PyPIC_Scatter_Gather):
     #@profile
-    def __init__(self, x_aper, y_aper, Dh=None, dx=None, dy=None, fftlib = 'pyfftw'):
-        
-		print 'Start PIC init.:'
-		print 'FFT, Open Boundary'
+    def __init__(self, x_aper, y_aper, Dh=None, dx=None, dy=None,
+                 fftlib='pyfftw', verbose=True):
+
+        self.verbose = verbose
+        if verbose:
+            print 'Start PIC init.:'
+            print 'FFT, Open Boundary'
 
 
-		if dx!=None and dy!=None:
-			assert(Dh==None)
+        if dx!=None and dy!=None:
+            assert(Dh==None)
 
-		elif Dh!=None:
-			assert(dx==None and dy==None)
-			dx = Dh
-			dy = Dh
+        elif Dh!=None:
+            assert(dx==None and dy==None)
+            dx = Dh
+            dy = Dh
 
-		else:
-			raise ValueError('Dh or dx and dy must be specified!!!')
-		
-		super(FFT_OpenBoundary, self).__init__(x_aper, y_aper, dx, dy)
+        else:
+            raise ValueError('Dh or dx and dy must be specified!!!')
 
-
-		nx = len(self.xg)
-		ny = len(self.yg)
+        super(FFT_OpenBoundary, self).__init__(x_aper, y_aper, dx, dy,
+                                               verbose=verbose)
 
 
-		mx = -dx / 2 + np.arange(nx + 1) * dx
-		my = -dy / 2 + np.arange(ny + 1) * dy
-		x, y = np.meshgrid(mx, my)
-		r2 = x ** 2 + y ** 2
-		# Antiderivative
-		tmpfgreen = -1 / 2 * (-3 * x * y + x * y * np.log(r2)
-				   + x * x * np.arctan(y / x) + y * y * np.arctan(x / y)) # * 2 / dx / dy
-				   
-		fgreen = np.zeros((2 * ny, 2 * nx))
-		# Integration and circular Green's function
-		fgreen[:ny, :nx] = tmpfgreen[1:, 1:] + tmpfgreen[:-1, :-1] - tmpfgreen[1:, :-1] - tmpfgreen[:-1, 1:]
-		fgreen[ny:, :nx] = fgreen[ny:0:-1, :nx]
-		fgreen[:ny, nx:] = fgreen[:ny, nx:0:-1]
-		fgreen[ny:, nx:] = fgreen[ny:0:-1, nx:0:-1]
-		
-		if fftlib == 'pyfftw':
-			try:
-				import pyfftw
-				print 'Using PyFFTW'
-				#prepare fftw's
-				tmprho = fgreen.copy()
-				fft_first = pyfftw.builders.fft(tmprho[:ny, :].copy(), axis = 1)
-				transf1 = (fgreen*(1.+1j))*0.
-				transf1[:ny, :] = fft_first(tmprho[:ny, :].copy())
-				fft_second = pyfftw.builders.fft(transf1.copy(), axis = 0)
-				fftphi_new = fft_second(transf1.copy())* fgreen
-				ifft_first = pyfftw.builders.ifft(fftphi_new.copy(), axis = 0)
-				itransf1 = ifft_first(fftphi_new.copy())
-				ifft_second = pyfftw.builders.ifft(itransf1[:ny, :].copy(), axis = 1)
+        nx = len(self.xg)
+        ny = len(self.yg)
 
-				def fft2(x):
-					tmp = (x*(1.+1j))*0.
-					tmp[:ny, :] = fft_first(x[:ny, :])
-					return fft_second(tmp)
-					
-				def ifft2(x):
-					tmp = ifft_first(x)
-					res = 0*x
-					res[:ny, :] = np.real(ifft_second(tmp[:ny, :]))
-					return res
-				
-				self.fft2 = fft2
-				self.ifft2 = ifft2
-				
-			except ImportError as err:
-				print 'Failed to import pyfftw'
-				print 'Got exception: ', err
-				print 'Using numpy fft'
-				self.fft2 = np.fft.fft2
-				self.ifft2 = np.fft.ifft2
-		elif fftlib == 'numpy':
-			print 'Using numpy FFT'
-			self.fft2 = np.fft.fft2
-			self.ifft2 = np.fft.ifft2
-		else:
-			raise ValueError('fftlib not recognized!!!!')
-			
-		self.fgreen = fgreen
-		self.fgreentr = np.fft.fft2(fgreen).copy()
-		self.rho = np.zeros((self.Nxg,self.Nyg))
-		self.phi = np.zeros((self.Nxg,self.Nyg))
-		self.efx = np.zeros((self.Nxg,self.Nyg))
-		self.efy = np.zeros((self.Nxg,self.Nyg))
-		self.Dh = Dh
-		self.nx = nx
-		self.ny = ny
-                        
 
-    #@profile    
+        mx = -dx / 2 + np.arange(nx + 1) * dx
+        my = -dy / 2 + np.arange(ny + 1) * dy
+        x, y = np.meshgrid(mx, my)
+        r2 = x ** 2 + y ** 2
+        # Antiderivative
+        tmpfgreen = -1 / 2 * (-3 * x * y + x * y * np.log(r2)
+                   + x * x * np.arctan(y / x) + y * y * np.arctan(x / y)) # * 2 / dx / dy
+
+        fgreen = np.zeros((2 * ny, 2 * nx))
+        # Integration and circular Green's function
+        fgreen[:ny, :nx] = tmpfgreen[1:, 1:] + tmpfgreen[:-1, :-1] - tmpfgreen[1:, :-1] - tmpfgreen[:-1, 1:]
+        fgreen[ny:, :nx] = fgreen[ny:0:-1, :nx]
+        fgreen[:ny, nx:] = fgreen[:ny, nx:0:-1]
+        fgreen[ny:, nx:] = fgreen[ny:0:-1, nx:0:-1]
+
+        if fftlib == 'pyfftw':
+            try:
+                import pyfftw
+                if self.verbose:
+                    print 'Using PyFFTW'
+                #prepare fftw's
+                tmprho = fgreen.copy()
+                fft_first = pyfftw.builders.fft(tmprho[:ny, :].copy(), axis = 1)
+                transf1 = (fgreen*(1.+1j))*0.
+                transf1[:ny, :] = fft_first(tmprho[:ny, :].copy())
+                fft_second = pyfftw.builders.fft(transf1.copy(), axis = 0)
+                fftphi_new = fft_second(transf1.copy())* fgreen
+                ifft_first = pyfftw.builders.ifft(fftphi_new.copy(), axis = 0)
+                itransf1 = ifft_first(fftphi_new.copy())
+                ifft_second = pyfftw.builders.ifft(itransf1[:ny, :].copy(), axis = 1)
+
+                def fft2(x):
+                    tmp = (x*(1.+1j))*0.
+                    tmp[:ny, :] = fft_first(x[:ny, :])
+                    return fft_second(tmp)
+
+                def ifft2(x):
+                    tmp = ifft_first(x)
+                    res = 0*x
+                    res[:ny, :] = np.real(ifft_second(tmp[:ny, :]))
+                    return res
+
+                self.fft2 = fft2
+                self.ifft2 = ifft2
+
+            except ImportError as err:
+                if self.verbose:
+                    print 'Failed to import pyfftw'
+                    print 'Got exception: ', err
+                    print 'Using numpy fft'
+                self.fft2 = np.fft.fft2
+                self.ifft2 = np.fft.ifft2
+        elif fftlib == 'numpy':
+            if self.verbose:
+                print 'Using numpy FFT'
+            self.fft2 = np.fft.fft2
+            self.ifft2 = np.fft.ifft2
+        else:
+            raise ValueError('fftlib not recognized!!!!')
+
+        self.fgreen = fgreen
+        self.fgreentr = np.fft.fft2(fgreen).copy()
+        self.rho = np.zeros((self.Nxg,self.Nyg))
+        self.phi = np.zeros((self.Nxg,self.Nyg))
+        self.efx = np.zeros((self.Nxg,self.Nyg))
+        self.efy = np.zeros((self.Nxg,self.Nyg))
+        self.Dh = Dh
+        self.nx = nx
+        self.ny = ny
+
+
+    #@profile
     def solve(self, rho = None, flag_verbose = False):
-		if rho == None:
-			rho = self.rho
+        if rho == None:
+            rho = self.rho
 
-		tmprho = 0.*self.fgreen
-		tmprho[:self.ny, :self.nx] = rho.T
+        tmprho = 0.*self.fgreen
+        tmprho[:self.ny, :self.nx] = rho.T
 
-		fftphi = self.fft2(tmprho) * self.fgreentr
-		
-		tmpphi = self.ifft2(fftphi)
-		self.phi = 1./(4. * np.pi * eps0)*np.real(tmpphi[:self.ny, :self.nx]).T
+        fftphi = self.fft2(tmprho) * self.fgreentr
 
-		self.efx[1:self.Nxg-1,:] = self.phi[0:self.Nxg-2,:] - self.phi[2:self.Nxg,:];  #central difference on internal nodes
-		self.efy[:,1:self.Nyg-1] = self.phi[:,0:self.Nyg-2] - self.phi[:,2:self.Nyg];  #central difference on internal nodes
+        tmpphi = self.ifft2(fftphi)
+        self.phi = 1./(4. * np.pi * eps0)*np.real(tmpphi[:self.ny, :self.nx]).T
 
-		
-		self.efy = self.efy/(2*self.dy)
-		self.efx = self.efx/(2*self.dx)
-        
-        
+        self.efx[1:self.Nxg-1,:] = self.phi[0:self.Nxg-2,:] - self.phi[2:self.Nxg,:];  #central difference on internal nodes
+        self.efy[:,1:self.Nyg-1] = self.phi[:,0:self.Nyg-2] - self.phi[:,2:self.Nyg];  #central difference on internal nodes
+
+
+        self.efy = self.efy/(2*self.dy)
+        self.efx = self.efx/(2*self.dx)
+
+
 
 
 
