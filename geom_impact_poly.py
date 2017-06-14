c7b569ca05ae80c1bd723c4452d80aaa19eed011
Adrian Oeftiger <oeftiger@cern.ch>
2017-06-14 17:52:29 +0200

    Bump version to 2.2.1

--- geom_impact_poly.py
+++ geom_impact_poly.py
@@ -7,7 +7,7 @@
 #     
 #     This file is part of the code:
 #                                                                                          
-#                   PyPIC Version 2.2.0                   
+#                   PyPIC Version 2.2.1                   
 #                  
 #                                                                       
 #     Author and contact:   Giovanni IADAROLA 
