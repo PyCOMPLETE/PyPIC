6ba7e58e4349c05a1b191fe1fbe1eeaf37a028d5
Adrian Oeftiger <oeftiger@cern.ch>
2017-06-19 10:39:23 +0200

    bump version to 2.2.2

--- change_version_number.py
+++ change_version_number.py
@@ -9,7 +9,7 @@ for filename in files:
         with open(filename) as fid:
             content=fid.read()
         if 'giovanni.iadarola@cern.ch' in content:
-            content=content.replace('PyPIC Version 2.2.1', 'PyPIC Version 2.2.1')
+            content=content.replace('PyPIC Version 2.2.2', 'PyPIC Version 2.2.2')
             with open(filename,'w') as fid:
                 fid.write(content)
 
