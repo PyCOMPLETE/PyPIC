8f1ba39b6e146aa332c1fa8df9c7ab3f2d01374d
Adrian Oeftiger <a.oeftiger@gmx.net>
2019-06-21 14:57:54 +0200

    bumped version number

--- change_version_number.py
+++ change_version_number.py
@@ -9,7 +9,7 @@ for filename in files:
         with open(filename) as fid:
             content=fid.read()
         if 'giovanni.iadarola@cern.ch' in content:
-            content=content.replace('PyPIC Version 2.4.0', 'PyPIC Version 2.4.0')
+            content=content.replace('PyPIC Version 2.4.1', 'PyPIC Version 2.4.1')
             with open(filename,'w') as fid:
                 fid.write(content)
 
