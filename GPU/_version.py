f3afe92a861053db5cd443f8cb42bf693b573697
Adrian Oeftiger <a.oeftiger@gmx.net>
2019-06-21 14:32:11 +0200

    fixing _version.py to properly work with top directory

--- GPU/_version.py
+++ GPU/_version.py
@@ -1,12 +1,14 @@
 import os, subprocess
 worktree = os.path.dirname(os.path.abspath(__file__))
-gitdir = worktree + '/.git/'
+gitdir = worktree + '/../.git/'
 try:
     __version__ = subprocess.check_output(
         'git --git-dir=' + gitdir + ' --work-tree=' +
         worktree + ' describe --long --dirty --abbrev=10 --tags', shell=True)
     __version__ = __version__.rstrip() # remove trailing \n
     __version__ = __version__[1:] # remove leading v
+    # make bytes object a string
+    __version__ = __version__.decode("utf-8") 
     # remove commit hash to conform to PEP440:
     split_ = __version__.split('-')
     __version__ = split_[0]
