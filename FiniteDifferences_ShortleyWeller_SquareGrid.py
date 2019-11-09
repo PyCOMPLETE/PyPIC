bc01dd7a8855a80e45c7337402619a02c5569c07
giadarol <giovanni.iadarola@cern.ch>
2019-11-09 13:05:45 +0100

    Some cleanup

--- FiniteDifferences_ShortleyWeller_SquareGrid.py
+++ FiniteDifferences_ShortleyWeller_SquareGrid.py
@@ -95,8 +95,8 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
         gradmod=abs(gx)+abs(gy);
         flag_border_mat=np.logical_and((gradmod>0), flag_outside_n_mat);
         flag_border_n = flag_border_mat.flatten()
-        
-        
+
+
         if include_solver:
             A=scsp.lil_matrix((Nxg*Nyg,Nxg*Nyg)); #allocate a sparse matrix
             Dx=scsp.lil_matrix((Nxg*Nyg,Nxg*Nyg)); #allocate a sparse matrix
@@ -108,35 +108,35 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
                 if np.mod(u, Nxg*Nyg//20)==0:
                     print ('Mat. assembly %.0f'%(float(u)/ float(Nxg*Nyg)*100)+"""%""")
                 if flag_inside_n[u]:
-                    
+
                     #Compute Shortley-Weller coefficients
                     if flag_inside_n[u-1]: #phi(i-1,j)
                         hw = Dh
                     else:
                         x_int,y_int,z_int,Nx_int,Ny_int, i_found_int = chamb.impact_point_and_normal(na(xn[u]), na(yn[u]), na(0.), na(xn[u-1]), na(yn[u-1]), na(0.), resc_fac=.995, flag_robust=False)
                         hw = np.abs(y_int[0]-yn[u])
-                        
+
                     if flag_inside_n[u+1]: #phi(i+1,j)
                         he = Dh
                     else:
                         x_int,y_int,z_int,Nx_int,Ny_int, i_found_int = chamb.impact_point_and_normal(na(xn[u]), na(yn[u]), na(0.), na(xn[u+1]), na(yn[u+1]), na(0.), resc_fac=.995, flag_robust=False)
                         he = np.abs(y_int[0]-yn[u])
-                    
+
                     if flag_inside_n[u-Nyg]: #phi(i,j-1)
                         hs = Dh
                     else:
                         x_int,y_int,z_int,Nx_int,Ny_int, i_found_int = chamb.impact_point_and_normal(na(xn[u]), na(yn[u]), na(0.), na(xn[u-Nyg]), na(yn[u-Nyg]), na(0.), resc_fac=.995, flag_robust=False)
                         hs = np.abs(x_int[0]-xn[u])
                         #~ print hs
-                    
+
                     if flag_inside_n[u+Nyg]: #phi(i,j+1)
                         hn = Dh
                     else:
                         x_int,y_int,z_int,Nx_int,Ny_int, i_found_int = chamb.impact_point_and_normal(na(xn[u]), na(yn[u]), na(0.), na(xn[u+Nyg]), na(yn[u+Nyg]), na(0.), resc_fac=.995, flag_robust=False)
                         hn = np.abs(x_int[0]-xn[u])
                         #~ print hn
-                    
-                    
+
+
                     # Build A matrix
                     if hn<Dh*tol_stem or hs<Dh*tol_stem or hw<Dh*tol_stem or he<Dh*tol_stem: # nodes very close to the bounday
                         A[u,u] =1.
@@ -148,9 +148,7 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
                         A[u,u+1]=2./(he*(hw+he));     #phi(i+1,j)
                         A[u,u-Nyg]=2./(hs*(hs+hn));    #phi(i,j-1)
                         A[u,u+Nyg]=2./(hn*(hs+hn));    #phi(i,j+1)
-                    
-                    
-                    
+
                     # Build Dx matrix
                     if hn<Dh*tol_der:
                         if hs>=Dh*tol_der:
@@ -164,8 +162,8 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
                         Dx[u,u] = (1./(2*hn)-1./(2*hs))
                         Dx[u,u-Nyg]=1./(2*hs)
                         Dx[u,u+Nyg]=-1./(2*hn)
-                        
-                        
+
+
                     # Build Dy matrix	
                     if he<Dh*tol_der:
                         if hw>=Dh*tol_der:
@@ -178,25 +176,25 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
                     else:
                         Dy[u,u] = (1./(2*he)-1./(2*hw))
                         Dy[u,u-1]=1./(2*hw)
-                        Dy[u,u+1]=-1./(2*he)				
+                        Dy[u,u+1]=-1./(2*he)
 
                 else:
                     # external nodes
                     A[u,u]=1.
 
 
-            flag_force_zero = flag_outside_n.copy()	
-            for ind in 	list_internal_force_zero:
+            flag_force_zero = flag_outside_n.copy()
+            for ind in list_internal_force_zero:
                 flag_force_zero[ind] = True
-                
+
             flag_force_zero_mat=np.reshape(flag_force_zero,(Nyg,Nxg),'F');
             flag_force_zero_mat=flag_force_zero_mat.T
-            
+
             print 'Internal nodes with 0 potential'
             print list_internal_force_zero
 
             A=A.tocsr() #convert to csr format
-            
+
             #Remove trivial equtions 
             diagonal = A.diagonal()
             N_full = len(diagonal)
@@ -206,7 +204,7 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
             Msel = scsp.lil_matrix((N_full, N_sel))
             for ii, ind in enumerate(indices_non_id):
                 Msel[ind, ii] =1.
-                
+
             Msel = Msel.tocsc()
 
             Asel = Msel.T*A*Msel
@@ -240,22 +238,20 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
 
             self.tol_der = tol_der
             self.tol_stem = tol_stem
-            
 
-            
             print 'Done PIC init.'
-            
+
         else:
-            
+
             self.solve = self._solve_for_states
-            
-        self.flag_inside_n_mat = np.logical_not(flag_outside_n_mat)			
+
+        self.flag_inside_n_mat = np.logical_not(flag_outside_n_mat)
         self.chamb = chamb
         self.rho = np.zeros((self.Nxg,self.Nyg));
         self.phi = np.zeros((self.Nxg,self.Nyg));
         self.efx = np.zeros((self.Nxg,self.Nyg));
         self.efy = np.zeros((self.Nxg,self.Nyg));
-                
+
     #@profile    
     def solve(self, rho = None, flag_verbose = False):
 
@@ -263,25 +259,25 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
             rho = self.rho
         self._solve_core(self, rho)
 
-        
+
     def gather(self, x_mp, y_mp):
-        
+
         if not (len(x_mp)==len(y_mp)):
             raise ValueError('x_mp, y_mp should have the same length!!!')
 
-        if len(x_mp)>0:    
+        if len(x_mp)>0:
             ## compute beam electric field
             Ex_sc_n, Ey_sc_n = iffb.int_field_border(x_mp,y_mp,self.bias_x,self.bias_y,self.Dh,
                                          self.Dh, self.efx, self.efy, self.flag_inside_n_mat)
-                       
+
         else:
             Ex_sc_n=0.
             Ey_sc_n=0.
-            
+
         return Ex_sc_n, Ey_sc_n
 
     def build_sparse_solver(self):
-        
+
         if self.sparse_solver == 'scipy_slu':
             print "Using scipy superlu solver..."
             luobj = ssl.splu(self.Asel.tocsc())
@@ -290,44 +286,44 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
             try:
                 import PyKLU.klu as klu
                 luobj = klu.Klu(self.Asel.tocsc())
-            except StandardError, e: 
+            except StandardError, e:
                 print "Got exception: ", e
                 print "Falling back on scipy superlu solver:"
                 luobj = ssl.splu(self.Asel.tocsc())
         else:
             raise ValueError('Solver not recognized!!!!\nsparse_solver must be "scipy_slu" or "PyKLU"\n')
-                
+
         self.luobj = luobj
-        
+
     def get_state_object(self):
-        state = FiniteDifferences_ShortleyWeller_SquareGrid(chamb=self.chamb, Dh=self.Dh, 
-                    sparse_solver = self.sparse_solver, tol_stem = self.tol_stem, tol_der = self.tol_der, 
+        state = FiniteDifferences_ShortleyWeller_SquareGrid(chamb=self.chamb, Dh=self.Dh,
+                    sparse_solver = self.sparse_solver, tol_stem = self.tol_stem, tol_der = self.tol_der,
                     include_solver=False)
-        
+
         state.rho = self.rho.copy()
         state.phi = self.phi.copy()
         state.efx = self.efx.copy()
         state.efy = self.efy.copy()
-        
+
         return state
-        
-        
+
+
     def solve_states(self, states):
-        
+
         states = np.atleast_1d(states)
         for ii in xrange(len(states)):
             state = states[ii]
             self._solve_core(state, state.rho)
-                
-                
+
+
     def _solve_core(self, state, rho):
-            
+
         b=-rho.flatten()/eps0;
-        b[(self.flag_force_zero)]=0; 
+        b[(self.flag_force_zero)]=0;
         b_sel = self.Msel_T*b
         phi_sel = self.luobj.solve(b_sel)
         phi = self.Msel*phi_sel
-        
+
         efx = self.Dx*phi
         efy = self.Dy*phi
         phi=np.reshape(phi,(self.Nxg,self.Nyg))
@@ -338,4 +334,3 @@ class FiniteDifferences_ShortleyWeller_SquareGrid(PyPIC_Scatter_Gather):
         state.phi = phi
 
 
-        
