a27bf2f18e6c11ea02b841047c37476d4464a17b
giadarol <giovanni.iadarola@cern.ch>
2020-01-27 18:12:18 +0100

    Change version number

--- change_version_number.py
+++ change_version_number.py
@@ -9,7 +9,7 @@ for filename in files:
         with open(filename) as fid:
             content=fid.read()
         if 'giovanni.iadarola@cern.ch' in content:
-            content=content.replace('PyPIC Version 2.4.4', 'PyPIC Version 2.4.4')
+            content=content.replace('PyPIC Version 2.4.5', 'PyPIC Version 2.4.5')
             with open(filename,'w') as fid:
                 fid.write(content)
 
