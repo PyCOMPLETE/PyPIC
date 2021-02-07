7979c90cad4e2285298a99bb24e76011f3c1b907
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-02-07 21:31:36 +0100

    Status

--- xfields/spacecharge.py
+++ xfields/spacecharge.py
@@ -1,17 +1,18 @@
 
 
 
-class SpaceChargeModulated2D(object):
+class SpaceCharge2D(object):
 
     def __init__(self, 
                  update_on_track=False, # Decides if frozen or soft-gaussian
                  apply_z_kick=True
                  transverse_field_map=None, 
                  longitudinal_profile=None,
-                 contex=None,
-                 )
+                 context=None,
+                 ):
+        pass
 
-class SpaceChargeModulated2DBiGaussian(SpaceChargeModulated2D):
+class SpaceCharge2DBiGaussian(SpaceChargeModulated2D):
 
     def __init__(self, 
                  update_on_track=False, # Decides if frozen or soft-gaussian
@@ -21,19 +22,46 @@ class SpaceChargeModulated2DBiGaussian(SpaceChargeModulated2D):
                  sigma_z=None, 
                  z_grid=None, dz=None,
                  z_interp_method='linear',
-                 contex=None,
-                 )
+                 context=None,
+                 ):
+        pass
 
-class SpaceChargeModulated2DInterpMap(SpaceChargeModulated2D):
+class SpaceCharge2DInterpMap(SpaceChargeModulated2D):
 
     def __init__(self, 
                  update_on_track=False, # Decides if frozen or kick
                  apply_z_kick=True
-                 sigma_x=None, sigma_y=None,
+                 rho=None, phi=None, 
+                 x_grid=None, y_grid=None,
+                 dx=None, dy=None,
+                 x_range=None, y_range=None,
+                 xy_interp_method='linear',
                  longitudinal_mode='Gaussian', 
                  sigma_z=None, 
                  z_grid=None, dz=None,
                  z_interp_method='linear',
                  contex=None,
+                 ):
+        pass
+
+class SpaceCharge3D(object):
+
+    def __init__(self, 
+                 update_on_track=False, # Decides if frozen or kick
+                 mode='2.5D',
+                 apply_z_kick=True, 
+                 rho=None, phi=None, 
+                 x_grid=None, y_grid=None, z_grid=None
+                 dx=None, dy=None, dz=None,
+                 x_range=None, y_range=None, z_range=None, 
+                 xy_interp_method='linear',
+                 z_interp_method='linear',
+                 context=None,
                  )
+        '''
+        Needed when transverse normalized distribution changes along z.
+
+        mode can be 2.5D or 3D
+        '''
+
 
