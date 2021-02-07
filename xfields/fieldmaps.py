731e0aedecae9b84d768f3bc250864ad7b15460a
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-02-07 16:12:46 +0100

    Status

--- xfields/fieldmaps.py
+++ xfields/fieldmaps.py
@@ -3,13 +3,14 @@ from abc import ABC, abstractmethod
 
 class FieldMap(ABC):
 
-    def __init__(self, context=None, solver=None, solver_type=None, **kwargs):
+    @abstractmethod
+    def __init__(self, context=None, solver=None, solver_type=None, 
+                 updatable=True, **kwargs):
 
         '''
         The init will have context argument, specipying the context
         in which wee store the state of the field map
         '''
-
         if solver is not None:
             'Check conmpatibility with grid'
             self.solver = solver
@@ -19,6 +20,7 @@ class FieldMap(ABC):
     @abstractmethod
     def generate_solver(self, solver_type):
         pass
+        return solver
 
     @abstractmethod
     def get_data_and_singleparticle_code(self):
@@ -38,9 +40,15 @@ class FieldMap(ABC):
         pass
 
     def update_rho(self, rho, reset=True):
+        
+        self._assert_updatable()
+        
         self.rho = rho.copy()
 
     def update_phi(self, phi, reset=True):
+        
+        self._assert_updatable()
+
         self.phi = phi.copy()
 
     @abstractmethod
@@ -48,8 +56,12 @@ class FieldMap(ABC):
         '''
         If reset is false charge density is added to the stored one
         '''
+        
+        self._assert_updatable()
 
     def update_phi_from_rho(self, solver=None):
+        
+        self._assert_updatable()
 
         if solver is None:
             if hasattr(self, 'solver'):
@@ -60,10 +72,15 @@ class FieldMap(ABC):
     def update_all_from_particles(x_p, y_p, z_p, ncharges_p, q0, reset=True,
                                   solver=None):
         
+        self._assert_updatable()
+
         self.update_rho_from_particles(
             x_p, y_p, z_p, ncharges_p, q0, reset=reset)
 
         self.update_phi_from_rho(solver=solver)
+
+    def _assert_updatable(self):
+        assert self.updatable, 'This FieldMap is not updatable!'
 
 
 class BiGaussianFieldMap(FieldMap):
@@ -100,7 +117,7 @@ class BiGaussianFieldMap(FieldMap):
 
     def update_rho_from_particles(x_p, y_p, z_p, ncharges_p, q0, reset=True):
 
-        assert reset, ('rho cannot be added for '
+        assert reset, ('rho cannot be added (reset must be True) for '
                       'for UpdatableBiGaussianFieldMap')
         # Basically updates sigma_rhos
 
@@ -125,6 +142,32 @@ class InterpolatedFieldMap(FieldMap):
         '''
 
         # 1D, 2D or 3D is inferred from the matrix size 
+        pass
+
+    def get_values_at_points(self,
+            x, y, z=0, 
+            return_rho=False, 
+            return_phi=False,
+            return_dphi_dx=False, 
+            return_dphi_dy=False, 
+            return_dphi_dz=False):
+        pass
+
+
+class InterpolatedFieldMapWithBoundary(FieldMap): 
+
+    def __init__(self, rho=None, phi=None, 
+                 x_grid=None, y_grid=None, z_grid=None
+                 dx=None, dy=None, dz=None,
+                 xy_interp_method='linear',
+                 z_interp_method='linear',
+                 boundary=None
+                 context=None):
+        '''
+        Does the Shortley-Weller interpolation close to the boundary.
+        Might need to force 2D and linear for now.
+        '''
+
         pass
 
     def get_values_at_points(self,
