d2d6138b13761c6806e60641416e86662dcb6878
giadarol <giovanni.iadarola@cern.ch>
2021-02-08 16:27:56 +0100

    All modules can be imported

--- xfields/beam_elements/spacecharge.py
+++ xfields/beam_elements/spacecharge.py
@@ -1,43 +1,42 @@
 
 
-
 class SpaceCharge2D(object):
 
-    def __init__(self, 
+    def __init__(self,
                  update_on_track=False, # Decides if frozen or soft-gaussian
-                 apply_z_kick=True
-                 transverse_field_map=None, 
+                 apply_z_kick=True,
+                 transverse_field_map=None,
                  longitudinal_profile=None,
                  context=None,
                  ):
         pass
 
-class SpaceCharge2DBiGaussian(SpaceChargeModulated2D):
+class SpaceCharge2DBiGaussian(SpaceCharge2D):
 
-    def __init__(self, 
+    def __init__(self,
                  update_on_track=False, # Decides if frozen or soft-gaussian
-                 apply_z_kick=True
+                 apply_z_kick=True,
                  sigma_x=None, sigma_y=None,
-                 longitudinal_mode='Gaussian', 
-                 sigma_z=None, 
+                 longitudinal_mode='Gaussian',
+                 sigma_z=None,
                  z_grid=None, dz=None,
                  z_interp_method='linear',
                  context=None,
                  ):
         pass
 
-class SpaceCharge2DInterpMap(SpaceChargeModulated2D):
+class SpaceCharge2DInterpMap(SpaceCharge2D):
 
-    def __init__(self, 
+    def __init__(self,
                  update_on_track=False, # Decides if frozen or kick
-                 apply_z_kick=True
-                 rho=None, phi=None, 
+                 apply_z_kick=True,
+                 rho=None, phi=None,
                  x_grid=None, y_grid=None,
                  dx=None, dy=None,
                  x_range=None, y_range=None,
                  xy_interp_method='linear',
-                 longitudinal_mode='Gaussian', 
-                 sigma_z=None, 
+                 longitudinal_mode='Gaussian',
+                 sigma_z=None,
                  z_grid=None, dz=None,
                  z_interp_method='linear',
                  contex=None,
@@ -46,22 +45,22 @@ class SpaceCharge2DInterpMap(SpaceChargeModulated2D):
 
 class SpaceCharge3D(object):
 
-    def __init__(self, 
+    def __init__(self,
                  update_on_track=False, # Decides if frozen or kick
                  mode='2.5D',
-                 apply_z_kick=True, 
-                 rho=None, phi=None, 
-                 x_grid=None, y_grid=None, z_grid=None
+                 apply_z_kick=True,
+                 rho=None, phi=None,
+                 x_grid=None, y_grid=None, z_grid=None,
                  dx=None, dy=None, dz=None,
-                 x_range=None, y_range=None, z_range=None, 
+                 x_range=None, y_range=None, z_range=None,
                  xy_interp_method='linear',
                  z_interp_method='linear',
                  context=None,
-                 )
+                 ):
         '''
         Needed when transverse normalized distribution changes along z.
 
         mode can be 2.5D or 3D
         '''
-
+        pass
 
