1323a2ce71c5d85a4de755ed83287cf3c73ecd6b
giadarol <giovanni.iadarola@cern.ch>
2021-02-14 09:56:30 +0100

    We have a sphere (not tested yet)

--- examples/000_sphere/000_sphere_test.py
+++ examples/000_sphere/000_sphere_test.py
@@ -1,4 +1,23 @@
 import sys
 sys.path.append('../../xfields/csrc')
 
+import numpy as np
+from numpy.random import rand
+
 import p2m_cpu
+
+center_xyz = np.array([0.1, 0.2, 0.3])
+radius = .5
+n_part_cube = 10000
+
+x_cube = radius*(2. * rand(n_part_cube) - 1.) + center_xyz[0]
+y_cube = radius*(2. * rand(n_part_cube) - 1.) + center_xyz[1]
+z_cube = radius*(2. * rand(n_part_cube) - 1.) + center_xyz[2]
+
+mask_sphere = ((x_cube - center_xyz[0])**2
+             + (y_cube - center_xyz[1])**2
+             + (z_cube - center_xyz[2])**2) < radius**2
+x = x_cube[mask_sphere]
+y = y_cube[mask_sphere]
+z = z_cube[mask_sphere]
+
