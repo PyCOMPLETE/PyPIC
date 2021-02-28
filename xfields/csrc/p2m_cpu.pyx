cf62434ed6e38658e619bbc4140ee3070e8df226
giadarol <giovanni.iadarola@cern.ch>
2021-02-28 10:27:48 +0100

    Trying to get rid of memoryview but doesn't work...

--- xfields/csrc/p2m_cpu.pyx
+++ xfields/csrc/p2m_cpu.pyx
@@ -34,14 +34,15 @@ def p2m(double[::1] x, double[::1] y, double[::1] z,
         &rho[0,0,0])
 
 from libc.stdlib cimport malloc, free
-
+import numpy as np
+cimport numpy as np
 def m2p(
         double[::1] x, double[::1] y, double[::1] z,
         double x0, double y0, double z0,
         double dx, double dy, double dz,
         int nx, int ny, int nz,
-        double[::1, :, :] mesh_quantity,
-        double[::1] particles_quantity):
+        np.ndarray mesh_quantity,
+        np.ndarray particles_quantity):
 
     cdef int n_maps = 1
     cdef double** mq_pointers= \
@@ -49,18 +50,26 @@ def m2p(
     cdef double** pq_pointers = \
       <double**>malloc(n_maps * sizeof(double*))
 
-    mq_pointers[0] = &(mesh_quantity[0,0,0])
-    pq_pointers[0] = &(particles_quantity[0])
+    cdef np.ndarray Acsc_data
+    cdef double* Acsc_data_ptr
+    Acsc_data = mesh_quantity.data
+    Acsc_data_ptr = <double*>Acsc_data.data
 
-    m2p_rectmesh3d(
-        len(x),
-        &x[0], &y[0],&z[0],
-        x0, y0, z0,
-        dx, dy, dz,
-        nx, ny, nz,
-        n_maps,
-        mq_pointers,
-        pq_pointers)
+    #cdef np.ndarray this_data
+    #this_data = mesh_quantity.data
+    #mq_pointers[0] = <double*>this_data.data
+    #this_data = particles_quantity.data
+    #pq_pointers[0] = <double*>this_data.data
+
+    # m2p_rectmesh3d(
+    #     len(x),
+    #     &x[0], &y[0],&z[0],
+    #     x0, y0, z0,
+    #     dx, dy, dz,
+    #     nx, ny, nz,
+    #     n_maps,
+    #     mq_pointers,
+    #     pq_pointers)
 
     free(mq_pointers)
     free(pq_pointers)
