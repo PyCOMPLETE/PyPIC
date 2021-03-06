cf179db821d0b0b63f7023d0d04fca161f9e9341
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-06 07:51:18 +0100

    cupy example with multiple kernels

--- examples/002_kernels_pyocl_cupy/000b_cupy_demo.py
+++ examples/002_kernels_pyocl_cupy/000b_cupy_demo.py
@@ -1,6 +1,10 @@
 import numpy as np
 import cupy as cp
 
+#######################
+# For a single kernel #
+#######################
+
 add_kernel = cp.RawKernel(r'''
 extern "C" __global__
 void my_add(const float* x1, const float* x2, float* y) {
@@ -12,3 +16,44 @@ x1 = cp.array(np.arange(25, dtype=np.float32).reshape(5, 5))
 x2 = cp.array(np.arange(25, dtype=np.float32).reshape(5, 5))
 y = cp.zeros((5, 5), dtype=cp.float32)
 add_kernel((5,), (5,), (x1, x2, y))  # grid, block and arguments
+
+#####################
+# For a full module #
+#####################
+
+loaded_from_source = r'''
+extern "C"{
+
+__global__ void test_sum(const float* x1, const float* x2, float* y, \
+                         unsigned int N)
+{
+    unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tid < N)
+    {
+        y[tid] = x1[tid] + x2[tid];
+    }
+}
+
+__global__ void test_multiply(const float* x1, const float* x2, float* y, \
+                              unsigned int N)
+{
+    unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (tid < N)
+    {
+        y[tid] = x1[tid] * x2[tid];
+    }
+}
+
+}'''
+module = cp.RawModule(code=loaded_from_source)
+ker_sum = module.get_function('test_sum')
+ker_times = module.get_function('test_multiply')
+N = 10
+x1 = cp.arange(N**2, dtype=cp.float32).reshape(N, N)
+x2 = cp.ones((N, N), dtype=cp.float32)
+y = cp.zeros((N, N), dtype=cp.float32)
+ker_sum((N,), (N,), (x1, x2, y, N**2))   # y = x1 + x2
+assert cp.allclose(y, x1 + x2)
+ker_times((N,), (N,), (x1, x2, y, N**2)) # y = x1 * x2
+assert cp.allclose(y, x1 * x2)
+
