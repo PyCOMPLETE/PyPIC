e35e8abcccb0356eafb4533940554bc46aaa39fe
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-06 07:42:47 +0100

    Added cupy kernel example

---
+++ examples/002_kernels_pyocl_cupy/000b_cupy_demo.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cupy as cp
+
+add_kernel = cp.RawKernel(r'''
+extern "C" __global__
+void my_add(const float* x1, const float* x2, float* y) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    y[tid] = x1[tid] + x2[tid];
+}
+''', 'my_add')
+x1 = cp.array(np.arange(25, dtype=np.float32).reshape(5, 5))
+x2 = cp.array(np.arange(25, dtype=np.float32).reshape(5, 5))
+y = cp.zeros((5, 5), dtype=cp.float32)
+add_kernel((5,), (5,), (x1, x2, y))  # grid, block and arguments
