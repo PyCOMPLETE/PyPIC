21218567f2363d07f694da37f0968dd3c66bd8fa
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-08 11:32:09 +0100

    Kernel accessed through platform

--- xfields/multiplatform/pocl.py
+++ xfields/multiplatform/pocl.py
@@ -1,5 +1,51 @@
 import numpy as np
+
+import pyopencl as cl
 import pyopencl.array as cla
+
+class MinimalDotDict(dict):
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+class XfPoclPlatform(object):
+
+    def __init__(self, pocl_context=None, command_queue=None):
+
+        if pocl_context is None:
+            pocl_context = cl.create_some_context()
+
+        if command_queue is None:
+            command_queue = cl.CommandQueue(pocl_context)
+
+        assert command_queue.context == pocl_context
+
+        self.pocl_context = pocl_context
+        self.command_queue = command_queue
+        self.kernels = MinimalDotDict()
+
+    def add_kernels(self, src_code='', src_files=[], kernel_descriptions={}):
+
+        src_content = src_code
+        for ff in src_files:
+            with open(ff, 'r') as fid:
+                src_content += ('\n\n' + fid.read())
+
+        prg = cl.Program(self.pocl_context, src_content).build()
+
+        ker_names = kernel_descriptions.keys()
+        for nn in ker_names:
+            kk = getattr(prg, nn)
+            aa = kernel_descriptions[nn]['args']
+            nt_from = kernel_descriptions[nn]['num_threads_from_arg']
+            aa_types, aa_names = zip(*aa)
+            self.kernels[nn] = XfPoclKernel(pocl_kernel=kk,
+                arg_names=aa_names, arg_types=aa_types,
+                num_threads_from_arg=nt_from,
+                command_queue=self.command_queue)
+
+
+
+
 
 class XfPoclKernel(object):
 
