da37816ceec6db95dde5adbbe736c0da74c42e77
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-03-08 10:33:32 +0100

    Moved kernel class

---
+++ xfields/multiplatform/pocl.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pyopencl.array as cla
+
+class XfPoclKernel(object):
+
+    def __init__(self, pocl_kernel, arg_names, arg_types,
+                 num_threads_from_arg, command_queue,
+                 wait_on_call=True):
+
+        assert (len(arg_names) == len(arg_types) == pocl_kernel.num_args)
+        assert num_threads_from_arg in arg_names
+
+        self.pocl_kernel = pocl_kernel
+        self.arg_names = arg_names
+        self.arg_types = arg_types
+        self.num_threads_from_arg = num_threads_from_arg
+        self.command_queue = command_queue
+        self.wait_on_call = wait_on_call
+
+    @property
+    def num_args(self):
+        return len(self.arg_names)
+
+    @property
+    def context(self):
+        return self.pocl_kernel.context
+
+    def __call__(self, **kwargs):
+        assert len(kwargs.keys()) == self.num_args
+        arg_list = []
+        for nn, tt in zip(self.arg_names, self.arg_types):
+            vv = kwargs[nn]
+            if np.issctype(tt):
+                assert np.isscalar(vv)
+                arg_list.append(tt(vv))
+            else:
+                assert isinstance(vv, cla.Array)
+                arg_list.append(vv.base_data[vv.offset:])
+
+        event = self.pocl_kernel(self.command_queue,
+                (kwargs[self.num_threads_from_arg],),
+                None, *arg_list)
+
+        if self.wait_on_call:
+            event.wait()
+
+        return event
